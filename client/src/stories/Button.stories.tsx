a5c722b1dff57577b29633eb84a330359fc104ec
Alexander Khvolis <alexander.khvolis@keysight.com>
2024-02-23 21:32:41 +0100

    Button story

--- client/src/stories/Button.stories.tsx
+++ client/src/stories/Button.stories.tsx
@@ -1,19 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import ResetButton from '../components/tic-tac-toe/components/reset-button';
+import { Provider } from 'react-redux';
+import { store } from '../app/store';
 
 const meta = {
   title: 'ResetButton',
   component: ResetButton,
   tags: ['autodocs'],
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
 } satisfies Meta<typeof ResetButton>;
+
+// A super-simple mock of the state of the store
+export const MockedState = {};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {};
-
-export const Secondary: Story = {};
-
-export const Large: Story = {};
-
-export const Small: Story = {};
